8cb4ac315f41bcc29c31a97d521a46f114d86935
gawen <15062876252@163.com>
2024-05-28 00:02:29 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,20 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author 刘家雯
+ * @version 1.0
+ */
+
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+}
